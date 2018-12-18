eb81b1c7b1d52412ac1c52cb9e292a76d282d8f1
alexis vabre <info@figure8labs.com>
2018-12-18 11:38:59 -0800

    added filter to address book

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI.js'
 
 class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+  
   render() {
     return (
       <div>
-        Hello World
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
       </div>
     )
   }
 }
 
-export default App;
+export default App
